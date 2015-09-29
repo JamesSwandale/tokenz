b849c3efb24c87007e6ddb9a137b47f510fcf2ef
James Swandale <James.Swandale@workshare.com>
2015-09-29 18:13:01 +0100

    Blank tests

---
+++ tests/app.spec.js
@@ -0,0 +1,38 @@
+"use strict";
+var chai = require('chai'),
+	assert = chai.assert,
+	should = require('should'),
+	request = require('supertest'),
+	app = '';
+
+
+describe('Tokens tests', function() {
+	describe('', function() {
+		it('', function(done) {
+		});
+		it('', function(done) {
+		});
+	})
+
+	describe('', function() {
+		it('', function(done) {
+		});
+		it('', function(done) {
+		});
+	})
+
+	describe('', function() {
+		it('', function(done) {
+		});
+		it('', function(done) {
+		});
+	})
+
+	describe('', function() {
+		it('', function(done) {
+		});
+		it('', function(done) {
+		});
+	})
+
+});
